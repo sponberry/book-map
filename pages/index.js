727928d0ed699780dd49f58d71e83f00654c0ebc
Abi Nottingham <abigailillustration@gmail.com>
2023-04-26 09:17:04 +0100

    adds working google map to project

--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,63 @@ import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 import Date from '../components/date';
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import { useState, useCallback } from 'react';
 
 export async function getStaticProps() {
+
+  // const loader = new Loader({
+  //   apiKey: process.env.mapsApiKey,
+  //   version: "weekly",
+  // });
+  
+  // loader.load().then(async () => {
+  //   const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+  //   map = new Map(document.getElementById("map") as HTMLElement, {
+  //     center: { lat: -34.397, lng: 150.644 },
+  //     zoom: 8,
+  //   });
+  // });
   const allPostsData = getSortedPostsData();
   return {
     props: {
       allPostsData,
     },
   };
+  //process.env.mapsApiKey
+  // PUT GMAPS REQ HERE
 }
 
 export default function Home({ allPostsData }) {
+  const containerStyle = {
+    width: '400px',
+    height: '400px'
+  };
+  
+  const center = {
+    lat: -3.745,
+    lng: -38.523
+  };
+
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.mapsApiKey,
+  });
+
+  const [map, setMap] = useState(null)
+
+  const onLoad = useCallback(function callback(map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map)
+  }, [])
+
+  const onUnmount = useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
   return (
     <Layout home>
       <Head>
@@ -28,6 +74,17 @@ export default function Home({ allPostsData }) {
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
+        { isLoaded 
+          ? <GoogleMap
+              mapContainerStyle={containerStyle}
+              center={center}
+              zoom={10}
+              onLoad={onLoad}
+              onUnmount={onUnmount}
+            />
+         : ''
+        }
+        
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
